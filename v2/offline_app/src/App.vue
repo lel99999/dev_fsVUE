31072ccce247f1057a76de26788cbff690d36da6
lel99999 <luan.le@cfpb.gov>
2022-02-11 19:56:28 -0500

    upd files

--- v2/offline_app/src/App.vue
+++ v2/offline_app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex w-screen h-screen text-gray-700">
     <div class="flex flex-col flex-shrink-1 w-150 border-r border-gray-300 bg-gray-100">
+      <img src="./assets/autosyscheck_logo2.png" />
       <!-- sidebar -->
         <!-- <v-treeview v-model="treeData" :treeTypes="treeTypes" @selected="selected" :openAll="openAll" :contextItems="contextItems" @contextSelected="contextSelected"></v-treeview> -->
         <SideTree></SideTree>
